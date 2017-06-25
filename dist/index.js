c2cd641a451018a3e45f5f87d0fa8d287d7caeb8
Sam Greenhalgh <sam.greenhalgh@condenastint.com>
2017-06-25 23:16:38 +0100

    Remove build artifacts

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const start_slack_bot_1 = require("./start-slack-bot");
-const bot = start_slack_bot_1.default();
-//# sourceMappingURL=index.js.map
